6eb4b8e3bb0497edd9ed2222201adcda0c4149d7
ddlee <cnlijiacheng96@gmail.com>
2016-04-24 11:11:24 +0800

    read data code archive, with time in POSIXct

---
+++ readdata.R
@@ -0,0 +1,5 @@
+full <-read.table("../Data/household_power_consumption.txt", sep=";", header=TRUE, na.strings = "?")
+full$time<-strptime(as.character(paste(full[,1],full[,2])), format="%d/%m/%Y %H:%M:%S")
+head(full)
+sub<-subset(full, full$time>=as.POSIXct("2007-02-01 00:00:00")&full$time<as.POSIXct("2007-02-03 00:00:00"))
+summary(sub)
